fab9a8a037048b2af879c61f16e763dea91b78c5
Aidas <aidas.jankauskas.dev@gmail.com>
2023-06-07 10:27:53 +0000

    Improving landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, useDisclosure } from '@chakra-ui/react'
+import { Box, Heading, SimpleGrid, useDisclosure } from '@chakra-ui/react'
 import { Button, Container, WalletModal } from 'components'
 import Generator from './components/Generator'
 
@@ -17,6 +17,23 @@ const Home = () => {
           Try it!
         </Button>
       </Box>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
+        <Box textAlign="center">
+          <Box className="fas fa-person fa-4x" mb={3} />
+          <Heading size="lg">Onboard Easily</Heading>
+          <Box mt={2}>Provide your users an easy access to your platform</Box>
+        </Box>
+        <Box textAlign="center">
+          <Box className="fas fa-key fa-4x" mb={3} />
+          <Heading size="lg">Validate Authenticity</Heading>
+          <Box mt={2}>Avoid bots and validate authenticity of your client using EAS</Box>
+        </Box>
+        <Box textAlign="center">
+          <Box className="fas fa-gift fa-4x" mb={3} />
+          <Heading size="lg">Connect Physically</Heading>
+          <Box mt={2}>Establish better trust with users by providing them physical card</Box>
+        </Box>
+      </SimpleGrid>
       <Generator />
       <WalletModal isOpen={isWalletOpen} onClose={onWalletClose} />
     </Container>
